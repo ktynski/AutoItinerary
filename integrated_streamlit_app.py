8d7ac55a738e1a7a4af1548a1b3b72fff1a0157a
ktynski <kristin@frac.tl>
2023-04-22 01:47:32 -0400

    Update integrated_streamlit_app.py

--- integrated_streamlit_app.py
+++ integrated_streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 from run import get_location_details, generate_coordinates, generate_concentric_circles, parse_tripadvisor_data, get_data_for_latlong_pairs, generate_gpt_itinerary, extract_itinerary_locations
 
 st.title('AutoItinerary')
@@ -9,7 +10,6 @@ user_preferences = {'destination': destination}
 
 your_tripadvisor_api_key = os.environ['your_tripadvisor_api_key']
 
-your_tripadvisor_api_key = st.text_input('Enter your TripAdvisor API key: (This should be kept secret)')
 
 def main(user_preferences, your_tripadvisor_api_key):
     # Refactor the AutoItinerary code to work with the Streamlit UI elements just created.
