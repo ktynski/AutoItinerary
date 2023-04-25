8aaecf975b6ac2abdcb9b9e12d4531474683e1be
ktynski <kristin@frac.tl>
2023-04-24 21:51:35 -0400

    Update Gapp.py

--- Gapp.py
+++ Gapp.py
@@ -79,19 +79,7 @@ def generate_gpt_itinerary(tripadvisor_data):
     
     
     report = []
-        # Looping over the response
-        for resp in openai.Completion.create(model='text-davinci-003',
-                                            prompt=user_input,
-                                            max_tokens=120, 
-                                            temperature = 0.5,
-                                            stream = True):
-            # join method to concatenate the elements of the list 
-            # into a single string, 
-            # then strip out any empty strings
-            report.append(resp.choices[0].text)
-            result = "".join(report).strip()
-            result = result.replace("\n", "")        
-            res_box.markdown(f'*{result}*') 
+   
             
             
             
